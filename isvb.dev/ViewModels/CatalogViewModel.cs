2a4e5a4d3a57d44dda911b1dbadef4300da2bf6e
hachiko93 <ana_93_bg@hotmail.com>
2016-02-13 12:28:10 +0100

    catalog view model added

---
+++ isvb.dev/ViewModels/CatalogViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isvb.dev.ViewModels
+{
+    class CatalogViewModel
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public String Description { get; set; }
+        
+    }
+}
