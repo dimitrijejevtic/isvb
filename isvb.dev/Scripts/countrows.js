5364c8d55fd9769f6a5c4ef1993d9c9466b221ad
Dimitrije Jevtic <mita250193@live.com>
2016-02-21 15:06:15 +0100

    count cart items patch

--- isvb.dev/Scripts/countrows.js
+++ isvb.dev/Scripts/countrows.js
@@ -1,11 +1,10 @@
-﻿var interval = 30000;  // 1000 = 1 second, 3000 = 3 seconds
+﻿var interval = 1000;  // 1000 = 1 second, 3000 = 3 seconds
 function doAjax() {
     $.ajax({
         type: 'POST',
         url: 'Cart/CountRows',
-        dataType: 'json',
         success: function (data) {
-            $(".myCart").val(data);
+            $(".myCart").text(data);
         },
         complete: function (data) {
 
