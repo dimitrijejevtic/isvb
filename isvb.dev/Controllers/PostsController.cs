3b9a9f623e222e76eefa39b70f23319311213250
Dimitrije Jevtic <mita250193@live.com>
2016-02-21 14:44:29 +0100

    Posts controler access liimit

--- isvb.dev/Controllers/PostsController.cs
+++ isvb.dev/Controllers/PostsController.cs
@@ -7,9 +7,11 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using isvb.dev;
+using isvb.dev.Models;
 
 namespace isvb.dev.Controllers
 {
+    [APIAuthorize(Roles ="Administrator, Owner")]
     public class PostsController : Controller
     {
         private EFModelContainer db = new EFModelContainer();
